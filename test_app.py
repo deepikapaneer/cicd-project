a2e802da2c799ed7eec44491f82fc64b6fb9408c
deepika paneer <deepselvam26@gmail.com>
2026-03-26 12:02:18 -0400

    intentionally break tests

--- test_app.py
+++ test_app.py
@@ -3,9 +3,9 @@ from app import app
 def test_home():
     client = app.test_client()
     res = client.get('/')
-    assert res.status_code == 200
+    assert res.status_code == 500     # intentionally wrong!
 
 def test_users():
     client = app.test_client()
     res = client.get('/users')
-    assert res.status_code == 200
+    assert res.status_code == 500      # intentionally wrong!
