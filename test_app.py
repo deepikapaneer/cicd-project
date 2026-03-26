85973fdd5146530626947e142aaf611321ee8a0b
deepika paneer <deepselvam26@gmail.com>
2026-03-26 12:04:28 -0400

    fix tests back to correct status code

--- test_app.py
+++ test_app.py
@@ -3,9 +3,9 @@ from app import app
 def test_home():
     client = app.test_client()
     res = client.get('/')
-    assert res.status_code == 500     # intentionally wrong!
+    assert res.status_code == 200     # fixed!
 
 def test_users():
     client = app.test_client()
     res = client.get('/users')
-    assert res.status_code == 500      # intentionally wrong!
+    assert res.status_code == 200      # fixed!
